c731501fef9929ff97d0cb167349471207653e42
Gerhard Lausser <gerhard.lausser@consol.de>
2017-04-21 01:34:42 +0200

    lexmark

--- plugins-scripts/Classes/Lexmark.pm
+++ plugins-scripts/Classes/Lexmark.pm
@@ -4,20 +4,11 @@ use strict;
 
 sub init {
   my $self = shift;
-  $self->get_snmp_tables('PRINTER-MIB', [
-    ['xxx', 'prtAlertTable', 'Monitoring::GLPlugin::SNMP::TableItem'],
-  ]);
-  $self->get_snmp_tables('LEXMARK-MPS-MIB', [
-    ['yyy', 'deviceAlertTable', 'Monitoring::GLPlugin::SNMP::TableItem'],
-  ]);
-  $self->get_snmp_tables('LEXMARK-PVT-MIB', [
-    ['zzz', 'prtgenAlertTable', 'Monitoring::GLPlugin::SNMP::TableItem'],
-  ]);
-
   if ($self->mode =~ /device::hardware::health/) {
     $self->analyze_and_check_environmental_subsystem('Classes::Lexmark::Components::EnvironmentalSubsystem');
-  } elsif ($self->mode =~ /device::consumables/) {
-    $self->analyze_and_check_battery_subsystem('Classes::Lexmark::Components::ConsumablesSubsystem');
+  } elsif ($self->mode =~ /device::printer::consumables/) {
+    #$self->analyze_and_check_battery_subsystem('Classes::Lexmark::Components::ConsumablesSubsystem');
+    $self->analyze_and_check_battery_subsystem('Classes::Lexmark::Component::PrinterSubsystem');
   } else {
     $self->no_such_mode();
   }
